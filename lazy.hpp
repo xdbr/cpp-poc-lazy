6817bdf29f9a7ee4ed3d8f8b46731de020b7390d
Daniel Bruder <daniel.bruder+git@gmail.com>
2013-11-24 12:46:28 +0100

    intermediate commit: one operation (re-calculating "bar") could/should be saved

--- lazy.hpp
+++ lazy.hpp
@@ -8,22 +8,30 @@ extern const bool TRACE;
 
 template<class T>
 struct lazy {
-    /// CTOR: Pass builder function to constructor
-    lazy(std::function<T()> fun) : fun_{fun} {}
-
-    /// Deref: Build value or simply just return the already built value
-    inline T operator*() {
+    lazy(std::string id, std::function<T()> fun) : fun_{fun}, id_{id} {}
+    // inline operator T () {
+    //     return **this;
+    // }
+    inline T operator*() const {
         if (!val_) {
-            if (TRACE) std::cout << "calculate value" << std::endl;
+            if (TRACE) std::cout << "lazy object with id = ("<<id_<<")" " needs some calculation " << std::endl;
             val_ = fun_();
         }
+        if (TRACE) std::cout << "returning value for lazy object with id_ = " << id_ << ": " << val_.get() << std::endl;
         return val_.get();
     }
-    
-    inline T operator+ (T v) { if (TRACE) std::cout << "start cascade of calculation" << std::endl; return **this + v; }
-    inline T operator- (T v) { if (TRACE) std::cout << "start cascade of calculation" << std::endl; return **this - v; }
-    inline T operator* (T v) { if (TRACE) std::cout << "start cascade of calculation" << std::endl; return **this * v; }
-    inline T operator/ (T v) { if (TRACE) std::cout << "start cascade of calculation" << std::endl; return **this / v; }
+
+    // lazy OP value -> (calculated) value
+    inline T operator+ (T v) { if (TRACE) std::cout << std::string(++depth, ' ') << "lazy OP value -> calculate value for object with id = " << id_ << " and RHS value = " << v << std::endl; if(depth > 0)--depth; return (**this) + (v); }
+    inline T operator- (T v) { if (TRACE) std::cout << std::string(++depth, ' ') << "lazy OP value -> calculate value for object with id = " << id_ << " and RHS value = " << v << std::endl; if(depth > 0)--depth; return (**this) - (v); }
+    inline T operator* (T v) { if (TRACE) std::cout << std::string(++depth, ' ') << "lazy OP value -> calculate value for object with id = " << id_ << " and RHS value = " << v << std::endl; if(depth > 0)--depth; return (**this) * (v); }
+    inline T operator/ (T v) { if (TRACE) std::cout << std::string(++depth, ' ') << "lazy OP value -> calculate value for object with id = " << id_ << " and RHS value = " << v << std::endl; if(depth > 0)--depth; return (**this) / (v); }
+
+    // lazy OP lazy -> (new) lazy
+    inline lazy<T> operator+ (lazy<T> v) { if (TRACE) { std::cout << std::string(++depth, ' ') << "lazy OP lazy -> lazy" << std::endl; } return lazy<T>( v.id_+"-a", [&]{ return (**this) + (*v); }); }
+    inline lazy<T> operator- (lazy<T> v) { if (TRACE) { std::cout << std::string(++depth, ' ') << "lazy OP lazy -> lazy" << std::endl; } return lazy<T>( v.id_+"-a", [&]{ return (**this) - (*v); }); }
+    inline lazy<T> operator* (lazy<T> v) { if (TRACE) { std::cout << std::string(++depth, ' ') << "lazy OP lazy -> lazy" << std::endl; } return lazy<T>( v.id_+"-a", [&]{ return (**this) * (*v); }); }
+    inline lazy<T> operator/ (lazy<T> v) { if (TRACE) { std::cout << std::string(++depth, ' ') << "lazy OP lazy -> lazy" << std::endl; } return lazy<T>( v.id_+"-a", [&]{ return (**this) / (*v); }); }
 
     /// Clear the value, force rebuilding
     inline void clear() {
@@ -31,7 +39,10 @@ struct lazy {
     }
 
     std::function<T()> fun_ = nullptr;  ///< Store builder
-    boost::optional<T> val_ = nullptr;  ///< Memoize value
+    mutable boost::optional<T> val_ = nullptr;  ///< Memoize value
     
+    int depth{0};
+    std::string id_;
 };
+
 #endif /* end of include guard: LAZY_HPP_V7J57G6G */
--- main.cpp
+++ main.cpp
@@ -7,32 +7,62 @@
 extern const bool TRACE = true; // get to see when values are calculated
 extern const bool PRINT = true;
 
-std::string build_some_string() {
-    return std::string{"foo"} + std::string{"bar"};
-}
-
 int main() {
-    lazy<int> foo( [&]{
-        return 2+3;
-    });
+    
+    if (TRACE) std::cout << "\n"
+    "//_____________ E X A M P L E  1 ______________________________________//"
+    << std::endl;
 
-    // will push out calculation to latest point possible
-     lazy<int> bar( [&]{
-        return foo * 3;
-    });
-
-    assert( *bar == (2+3)*3 );          // value will be calculated
-    std::cout << *bar << std::endl;     // no need for re-calculation
-
-    lazy<std::string> foo_string( [&]{
+    lazy<std::string> foo_string( "foo_string", []{
         return std::string{"foo"};
     });
     
-    lazy<std::string> foobar_string( [&]{
-        return foo_string + std::string{"bar"};
+    lazy<std::string> bar_string( "bar_string", []{
+        return std::string{"bar"};
     });
     
-    std::cout << *foobar_string << std::endl;
+    lazy<std::string> foobarbaz_string( "foobarbaz_string", [&]{
+        return foo_string + bar_string + std::string{"baz"};
+    });
+    
+    // only now kick off the cascade
+    std::cout << *foobarbaz_string << std::endl;
+    std::cout << std::endl << "CLEAR" << std::endl << std::endl;
+    foobarbaz_string.clear();
+    std::cout << *foobarbaz_string << std::endl; // no need for re-calculation
 
+    #if 0
+    // assert( *foobarbaz_string == "foobarbaz" );
+    
+    if (TRACE) std::cout << "\n"
+    "//_____________ E X A M P L E  2 ______________________________________//"
+    << std::endl;
+    lazy<int> one( "one", []{
+        return 1; // in real life, this would be complex & long-running
+    });
+
+    // push out calculation to latest point possible
+    lazy<int> two( "two", [&]{
+        return one + 1;
+    });
+
+    // will push out calculation to even more 'latest point possible'
+    lazy<int> four( "four", [&]{
+        return one + two + 1;
+    });
+
+    // now start calculation (possibly a cascade of calculations)
+    std::cout << *four << std::endl;
+
+    assert( *four == 4 );          // no need for re-calculation
+    
+    if (TRACE) std::cout << "\n"
+    "//_____________ E X A M P L E  3 ______________________________________//"
+    << std::endl;
+    
+    four.clear();
+    
+    std::cout << *four << std::endl;
+    #endif
     return 0;
 }
