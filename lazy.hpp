79a040e42058f8562c59e8a534c57721ff73ab49
Daniel Bruder <daniel.bruder+git@gmail.com>
2013-11-24 13:00:02 +0100

    let there be debug

--- lazy.hpp
+++ lazy.hpp
@@ -6,15 +6,51 @@
 
 extern const bool TRACE;
 
+#ifndef DODEBUG
+template<class T>
+struct lazy {
+    lazy(std::function<T()> fun) : fun_{fun} {}
+
+    inline T operator*() const {
+        if(!val_) val_ = fun_();
+        return val_.get();
+    }
+
+    // lazy OP value -> (calculated) value
+    inline T operator+ (T v) { return (**this) + (v); }
+    inline T operator- (T v) { return (**this) - (v); }
+    inline T operator* (T v) { return (**this) * (v); }
+    inline T operator/ (T v) { return (**this) / (v); }
+
+    // lazy OP lazy -> (new) lazy
+    inline lazy<T> operator+ (lazy<T> v) { return lazy<T>( [&]{ return (**this) + (*v); }); }
+    inline lazy<T> operator- (lazy<T> v) { return lazy<T>( [&]{ return (**this) - (*v); }); }
+    inline lazy<T> operator* (lazy<T> v) { return lazy<T>( [&]{ return (**this) * (*v); }); }
+    inline lazy<T> operator/ (lazy<T> v) { return lazy<T>( [&]{ return (**this) / (*v); }); }
+
+    /// Clear the value, force rebuilding
+    inline void clear() {
+        val_.reset();
+    }
+
+    const   std::function<T()> fun_ = nullptr;  ///< Store builder
+    mutable boost::optional<T> val_ = nullptr;  ///< Memoize value
+};
+
+#else /*DODEBUG*/
+
 template<class T>
 struct lazy {
     lazy(std::string id, std::function<T()> fun) : fun_{fun}, id_{id} {}
+    
+    // might be useful?
     // inline operator T () {
     //     return **this;
     // }
     inline T operator*() const {
+        if (val_) if (TRACE) std::cout << "value for object with id_ = " << id_ << " is calculated already" << std::endl;
         if (!val_) {
-            if (TRACE) std::cout << "lazy object with id = ("<<id_<<")" " needs some calculation " << std::endl;
+            if (TRACE) std::cout << "lazy object with id = ("<<id_<<")" " needs calculation " << std::endl;
             val_ = fun_();
         }
         if (TRACE) std::cout << "returning value for lazy object with id_ = " << id_ << ": " << val_.get() << std::endl;
@@ -44,5 +80,7 @@ struct lazy {
     int depth{0};
     std::string id_;
 };
+
+#endif /*DODEBUG*/
 
 #endif /* end of include guard: LAZY_HPP_V7J57G6G */
--- main.cpp
+++ main.cpp
@@ -8,7 +8,58 @@ extern const bool TRACE = true; // get to see when values are calculated
 extern const bool PRINT = true;
 
 int main() {
-    
+
+#ifndef DODEBUG
+    //_____________ E X A M P L E  1 ______________________________________//
+
+    lazy<std::string> foo_string( []{
+        return std::string{"foo"};
+    });
+
+    lazy<std::string> bar_string( []{
+        return std::string{"bar"};
+    });
+
+    lazy<std::string> foobarbaz_string( [&]{
+        return foo_string + bar_string + std::string{"baz"};
+    });
+
+    // only now kick off the cascade
+    std::cout << *foobarbaz_string << std::endl;
+
+    assert( *foobarbaz_string == "foobarbaz" );  // no need for re-calculation
+
+
+    //_____________ E X A M P L E  2 ______________________________________//
+
+    lazy<int> one( []{
+        return 1; // in real life, this would be complex & long-running
+    });
+
+    // push out calculation to latest point possible
+    lazy<int> two( [&]{
+        return one + 1;
+    });
+
+    // will push out calculation to even more 'latest point possible'
+    lazy<int> four( [&]{
+        return one + two + 1;
+    });
+
+    // now start calculation (possibly a cascade of calculations)
+    std::cout << *four << std::endl;
+
+    assert( *four == 4 );          // no need for re-calculation
+
+    //_____________ E X A M P L E  3 ______________________________________//
+
+    four.clear();           // force re-calculation
+
+    assert( *four == 4 );   // will need re-calculation
+
+
+#else /*DODEBUG*/
+
     if (TRACE) std::cout << "\n"
     "//_____________ E X A M P L E  1 ______________________________________//"
     << std::endl;
@@ -16,24 +67,24 @@ int main() {
     lazy<std::string> foo_string( "foo_string", []{
         return std::string{"foo"};
     });
-    
+
     lazy<std::string> bar_string( "bar_string", []{
         return std::string{"bar"};
     });
-    
+
     lazy<std::string> foobarbaz_string( "foobarbaz_string", [&]{
         return foo_string + bar_string + std::string{"baz"};
     });
-    
+
     // only now kick off the cascade
     std::cout << *foobarbaz_string << std::endl;
     std::cout << std::endl << "CLEAR" << std::endl << std::endl;
     foobarbaz_string.clear();
     std::cout << *foobarbaz_string << std::endl; // no need for re-calculation
 
-    #if 0
+    // #if 0
     // assert( *foobarbaz_string == "foobarbaz" );
-    
+
     if (TRACE) std::cout << "\n"
     "//_____________ E X A M P L E  2 ______________________________________//"
     << std::endl;
@@ -55,14 +106,16 @@ int main() {
     std::cout << *four << std::endl;
 
     assert( *four == 4 );          // no need for re-calculation
-    
+
     if (TRACE) std::cout << "\n"
     "//_____________ E X A M P L E  3 ______________________________________//"
     << std::endl;
-    
+
     four.clear();
-    
+
     std::cout << *four << std::endl;
-    #endif
+    // #endif /*0*/
+#endif /*DODEBUG*/
+
     return 0;
 }
